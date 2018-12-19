3f17327f08a4792787a1fffd31e6c1b81e765163
Kasim Ridwan <kasimsheyi20@gmail.com>
2018-12-18 18:49:16 -0800

    new changes

--- application/views/result1.php
+++ application/views/result1.php
@@ -41,7 +41,7 @@
                             	</div>
                                 
                             </div>
-                            <br><br><br>
+                            <br><br><br><br><br><br><br><br>
                         </div>
                     </div>
                 </div>
